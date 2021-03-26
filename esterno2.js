be5e55025e9bcb8a538c583dd99a8ca2f5bc92ba
dbaratta147 <dbaratta@enetworks.it>
2021-03-26 17:52:30 +0100

    fizzbuzz

--- esterno2.js
+++ esterno2.js
@@ -10,3 +10,22 @@ function addCar() {
     container_block = document.getElementById("carList");
     container_block.appendChild(block_to_insert);
 }
+
+function fizzbuzz() {
+    for (var i = 0; i <= 100; ++i) {
+        const div3 = i % 3 === 0;
+        const div5 = i % 5 === 0;
+
+        if (div3 && div5) {
+            console.log(`FizzBuzz`);
+        } else if (div3) {
+            console.log(`Fizz`);
+        } else if (div5) {
+            console.log(`Buzz`);
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+fizzbuzz()
