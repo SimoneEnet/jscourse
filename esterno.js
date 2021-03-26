8d8f3251b9d9a50c141973ac84f294c8e1cd01dc
ENET-148\Utente <nlatela@enetworks.it>
2021-03-26 15:53:22 +0100

    Esercizio

--- esterno.js
+++ esterno.js
@@ -1,9 +1,23 @@
 "use strict";
-var a = { value: 1 };
-var b = "ciao" + " " + "ok";
+/*  creare una pagina html con una lista di auto, questa lista viene mostrata dove ogni elemento è un div, uno sotto l'altro.
+    creare un oggetto javascript Car
+    serve una form per aggiungere un oggetto Car alla lista, che verrà anche aggiunto come div
+    ogni div deve mostrare le proprietà dell'oggetto Car, e deve avere anche un bottone per rimuovere l'elemento */
 
-function myFunction() {
-  var c = a;
-  console.log("ciao");
-  console.log(JSON.stringify(c));
+// Creazione oggetto Car
+var auto = {
+	marca: "Fiat",
+	modello: "Punto",
+	colore: "Grigio",
+	alimentazione: "benzina",
+	anno: 2006,
+	cavalli: 65,
+};
+
+// Funzione Aggiungi Auto
+function aggiungiAuto() {
+	var div = document.createElement("div");
+	var nuovaAuto = document.createTextNode("Auto 3");
+	div.appendChild(nuovaAuto);
+	document.getElementById("Auto").appendChild(div);
 }
