6cac361a3df5ce2f6b4c626a52251ef83ef562f6
Alin <adendiu@enetworks.it>
2021-03-26 15:51:53 +0100

    JavascriptCar

--- esterno.js
+++ esterno.js
@@ -1,9 +1,19 @@
 "use strict";
-var a = { value: 1 };
-var b = "ciao" + " " + "ok";
+  var block_to_insert ;
+  var container_block ;
+  
+  function addCar(){
+    
+  var modelloMacchina =document.getElementById("carModel").value;
+  var coloreMacchina = document.getElementById("carColor").value;
+  block_to_insert = document.createElement( "div" );
+  block_to_insert.innerHTML = modelloMacchina + " " + coloreMacchina + "<button  type='submit' onclick='removeElement()'>Delete</button>";
+  
+  container_block = document.getElementById( "carList" );
+  container_block.appendChild( block_to_insert );
+}
 
-function myFunction() {
-  var c = a;
-  console.log("ciao");
-  console.log(JSON.stringify(c));
+
+function removeElement(){
+  container_block.removeChild(container_block.childNodes[0]); 
 }
