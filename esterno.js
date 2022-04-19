8d8c100e0ece03f03b9e3b610914ca2bfcd5e3c0
SimoneEnet <scasarosa@enetworks.it>
2022-04-19 16:37:49 +0200

    update

--- esterno.js
+++ esterno.js
@@ -1,17 +1,22 @@
 "use strict";
 import * as car from "./car.js";
 
-let ita = new Map().set("carName", "auto");
-let eng = new Map().set("carName", "car");
+let ita = new Map()
+  .set("carName", "auto")
+  .set("marName", "Marca")
+  .set("modName", "Modello")
+  .set("colName", "Colore");
+let eng = new Map()
+  .set("carName", "car")
+  .set("marName", "Maker")
+  .set("modName", "Model")
+  .set("colName", "Color");
+let currLang = navigator.language === "it" ? ita : eng;
 
 let a = 1;
 
 let cars = [
-  new car.Car(
-    "qwe1",
-    navigator.language === "it" ? ita.get("carName") : eng.get("carName"),
-    "123"
-  ),
+  new car.Car("qwe1", currLang.get("carName"), "123"),
   new car.Car("qwe" + a + "2", "qwe", "123"),
   new car.Car(
     `qw
@@ -42,18 +47,18 @@ function createCarList() {
   cars.forEach((c, i) => {
     let block_to_insert = document.createElement("div");
     let title = document.createElement("h1");
-    title.innerHTML = "Element " + i;
+    title.innerHTML = `Element ${c.marca}-${c.modello}`;
     block_to_insert.appendChild(title);
     block_to_insert.appendChild(
-      document.createTextNode("marca: " + c.marca)
+      document.createTextNode(`${currLang.get("marName")}: ${c.marca}`)
     );
     block_to_insert.appendChild(document.createElement("br"));
     block_to_insert.appendChild(
-      document.createTextNode("modello: " + c.modello)
+      document.createTextNode(`${currLang.get("modName")}: ${c.modello}`)
     );
     block_to_insert.appendChild(document.createElement("br"));
     block_to_insert.appendChild(
-      document.createTextNode("colore: " + c.colore)
+      document.createTextNode(`${currLang.get("colName")}: ${c.colore}`)
     );
     block_to_insert.appendChild(document.createElement("br"));
     let button = document.createElement("button");
